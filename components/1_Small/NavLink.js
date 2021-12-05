0e2698b5e9614e6492e6199300445375dfe62a55
ragnsanvalla <ragnsanfv@gmail.com>
2021-12-05 16:16:28 +0100

    Fikset logikk for styling av current nav item

--- components/1_Small/NavLink.js
+++ components/1_Small/NavLink.js
@@ -1,10 +1,21 @@
+import { useRouter } from "next/dist/client/router";
+import React from "react";
 import Link from "next/link";
 import { camelCase } from "../helpers/toCamelCase";
 
-export const NavLink = ({ children, href }) => {
+export const NavLink = ({ children, href }, props) => {
+  const router = useRouter();
+
+  console.log("href " + href);
+
   return (
-    <Link href={href} className="">
-      <a className="ml-24 nav-hover">{camelCase(children)}</a>
+    <Link href={href} passHref>
+      <a
+        pathName={router.pathname}
+        className={`${`${href == "/" ? href : `/${href}`}` == router.pathname && "nav-current-page"} ml-24 nav-hover`}
+      >
+        {camelCase(children)}
+      </a>
     </Link>
   );
 };
