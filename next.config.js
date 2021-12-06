d2fa6c1214472ced33a3113eaed86f0bee44aff7
ragnsanvalla <ragnsanfv@gmail.com>
2021-12-06 15:40:47 +0100

    We try again

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ module.exports = {
   swcMinify: true, //uses SWC instead of Terser for minyfying JavaScript
   images: {
     domains: ["images.ctfassets.net"],
-    // loader: "images.ctfassets.net",
     path: `https://images.ctfassets.net/${process.env.NEXT_CONTENTFUL_SPACE_ID}`,
   },
   trailingSlash: true,
