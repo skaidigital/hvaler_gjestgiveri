578f7ff5286cce0164d1551833ad8f6e4e16f854
MrElgheim <petter.elgheim@gmail.com>
2021-12-13 16:37:12 +0100

    Endre id

--- components/1_Small/contentfulFetch.js
+++ components/1_Small/contentfulFetch.js
@@ -1,6 +1,6 @@
 export async function fetchContent(query) {
-  const space = process.env.NEXT_PUBLIC_SPACE_ID;
-  const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
+  const space = process.env.NEXT_CONTENTFUL_SPACE_ID;
+  const accessToken = process.env.NEXT_CONTENTFUL_ACCESS_TOKEN;
 
   try {
     const res = await fetch(
